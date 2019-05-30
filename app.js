b6215bb6642aba99ab70899878034b4036b647d2
pysin9 <pysin9@gmail.com>
2019-05-30 20:44:04 +0800

    Added dependancies

--- app.js
+++ app.js
@@ -1,41 +1,84 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+/*
+* 'require' is similar to import used in Java and Python. It brings in the libraries required to be used
+* in this JS file.
+* */
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+/*
+* Loads routes file main.js in routes directory. The main.js determines which function
+* will be called based on the HTTP request and URL.
+*/
+const mainRoute = require('./routes/main');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+/*
+* Creates an Express server - Express is a web application framework for creating web applications
+* in Node JS.
+*/
+const app = express();
 
-var app = express();
+// Handlebars Middleware
+/*
+* 1. Handlebars is a front-end web templating engine that helps to create dynamic web pages using variables
+* from Node JS.
+*
+* 2. Node JS will look at Handlebars files under the views directory
+*
+* 3. 'defaultLayout' specifies the main.handlebars file under views/layouts as the main template
+*
+* */
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main' // Specify default template views/layout/main.handlebar 
+}));
+app.set('view engine', 'handlebars');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// Body parser middleware to parse HTTP body in order to read HTTP data
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+app.use(bodyParser.json());
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// Creates static folder for publicly accessible HTML, CSS and Javascript files
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Method override middleware to use other HTTP methods such as PUT and DELETE
+app.use(methodOverride('_method'));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Enables session to be stored using browser's Cookie ID
+app.use(cookieParser());
+
+// To store session information. By default it is stored as a cookie on browser
+app.use(session({
+	key: 'Neworganics_session',
+	secret: 'tojiv',
+	resave: false,
+	saveUninitialized: false,
+}));
+
+// Place to define global variables - not used in practical 1
+app.use(function (req, res, next) {
+	next();
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// Use Routes
+/*
+* Defines that any root URL with '/' that Node JS receives request from, for eg. http://localhost:5000/, will be handled by
+* mainRoute which was defined earlier to point to routes/main.js
+* */
+app.use('/', mainRoute); // mainRoute is declared to point to routes/main.js
+// This route maps the root URL to any path defined in main.js
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+/*
+* Creates a unknown port 5000 for express server since we don't want our app to clash with well known
+* ports such as 80 or 8080.
+* */
+const port = 5000;
+
+// Starts the server and listen to port 5000
+app.listen(port, () => {
+	console.log(`Server started on port ${port}`);
 });
-
-module.exports = app;
