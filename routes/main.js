01df825b2ad0928ec16e24239e2f7ad3f2938fde
pysin9 <pysin9@gmail.com>
2019-05-31 22:04:37 +0800

    routes to login/register

--- routes/main.js
+++ routes/main.js
@@ -3,13 +3,21 @@ var router = express.Router();
 
 
 /* GET index */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   const title = "NewOrganics";
   res.render('index', { title: title });
 });
-
+router.get('/Login', (req, res) => {
+  const title = 'Login';
+  res.render('user/login', { title: title })
+});
+/*User Register*/
+router.get('/Register', (req, res) => {
+  const title = 'Register';
+  res.render('user/register', { title: title })
+})
 /* GET quiz */
-router.get('/quiz', function(req, res) {
+router.get('/quiz', function (req, res) {
   const title = "Quiz";
   res.render('quiz/quiz', { title: title });
 });
