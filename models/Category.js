42aa5f28951e18220e85346729509464e1e3a834
pysin9 <pysin9@gmail.com>
2019-07-01 12:34:19 +0800

    confirm delete fixed

--- models/Category.js
+++ models/Category.js
@@ -1,67 +1,10 @@
 const Sequelize = require('sequelize');
 const db = require('../config/DBConfig');
 const Category = db.define('category', {
-    fruits: {
+    catName: {
         type: Sequelize.STRING
     },
-    herbs: {
-        type: Sequelize.STRING
-    },
-    vegetables: {
-        type: Sequelize.STRING
-    },
-    milk:{
-        type: Sequelize.STRING
-    },
-    yogurt:{
-        type: Sequelize.STRING
-    },
-    butter:{
-        type: Sequelize.STRING
-    },
-    cheese:{
-        type: Sequelize.STRING
-    },
-    juice:{
-        type: Sequelize.STRING
-    },
-    soy:{
-        type: Sequelize.STRING
-    },
-    tea:{
-        type: Sequelize.STRING
-    },
-    chicken:{
-        type: Sequelize.STRING
-    },
-    pork:{
-        type: Sequelize.STRING
-    },
-    beef:{
-        type: Sequelize.STRING
-    },
-    fish:{
-        type: Sequelize.STRING
-    },
-    personal:{
-        type: Sequelize.STRING
-    },
-    healing:{
-        type: Sequelize.STRING
-    },
-    supplements:{
-        type: Sequelize.STRING
-    },
-    crackers:{
-        type: Sequelize.STRING
-    },
-    nuts:{
-        type: Sequelize.STRING
-    },
-    candy:{
-        type: Sequelize.STRING
-    },
-    energy:{
+    catType: {
         type: Sequelize.STRING
     },
 });
